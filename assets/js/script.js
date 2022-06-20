fc7feeec7f96f1a680105ce50145dbc76ac90b74
valcomp <valerie.aleida.lopez@gmail.com>
2022-06-20 17:33:46 -0500

    reverting to original state

--- assets/js/script.js
+++ assets/js/script.js
@@ -192,9 +192,7 @@ $("#search-btn").on("click", function(event){
         currentCity = cityWords.join(" ");
 
         console.log(currentCity);
-        if(currentCity){
-            cityHistory.push(currentCity);
-        }
+        cityHistory.push(currentCity);
         //Add the city name into the banner text
         $("#cityName").text(currentCity);    
 
