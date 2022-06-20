35be7f341b32023fbe3a947a009b4628cc8187bc
valcomp <valerie.aleida.lopez@gmail.com>
2022-06-20 00:09:50 -0500

    set up api fetch request to get daily weather information

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var history = [];
 var dateTime =luxon.DateTime;
 var dT= dateTime.now();
 var currentCity = "Austin";
+var lat, long;
+var api = 'dfecf031e9c329254bf19d276d4240d8';
 
 //create array of objects for week
 var week = [
@@ -12,6 +14,7 @@ var week = [
         dateYear: dT.year,
         temp: "",
         wind: "",
+        humidity: "",
         uvindex: ""
     },
     {
@@ -21,7 +24,7 @@ var week = [
         dateYear: dT.plus({days:1}).year,
         temp: "",
         wind: "",
-        uvindex: ""
+        humidity: "",
     },
     {
         index: 3,
@@ -30,7 +33,7 @@ var week = [
         dateYear: dT.plus({days:2}).year,
         temp: "",
         wind: "",
-        uvindex: ""
+        humidity: "",
     },
     {
        index: 4,
@@ -39,7 +42,7 @@ var week = [
        dateYear: dT.plus({days:3}).year,
        temp: "",
        wind: "",
-       uvindex: ""
+       humidity: "",
     },
     {
         index: 5,
@@ -48,7 +51,7 @@ var week = [
         dateYear: dT.plus({days:4}).year,
         temp: "",
         wind: "",
-        uvindex: ""
+        humidity: "",
     },
     {
         index: 6,
@@ -57,10 +60,9 @@ var week = [
         dateYear: dT.plus({days:5}).year,
         temp: "",
         wind: "",
-        uvindex: ""
+        humidity: "",
     },
 ]
-
 
 //get any past history inputs from the local storage
 var loadHistory = function(){
@@ -105,6 +107,55 @@ var setDates = function(){
     }
 }
 
+//In order to use the openweather api to get weather stats, we need to get the lat and lon coordinates of the cities entered
+var getLocationApi = function(city){
+    var requestLocationUrl = 'https://api.openweathermap.org/geo/1.0/direct?q='+currentCity+'&limit=1&appid=' + api;
+
+    fetch(requestLocationUrl)
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function (data) {
+            lat = data[0].lat;
+            long = data[0].lon;
+            getWeather();
+        });
+}
+
+//uses the oneCall weather API to get the stats for each day of the week
+var getWeather = function(){
+    var requestCurrentUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat='+lat+'&lon='+long+'&exclude=hourly,minutely,alerts&units=imperial&appid='+ api;
+
+    fetch(requestCurrentUrl)
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            for(var i=0; i<6;i++){
+                week[i].temp = data.daily[i].temp.day;
+                week[i].wind = data.daily[i].wind_speed;
+                week[i].humidity = data.daily[i].humidity;
+                week[i].uvindex = data.daily[i].uvi;
+            }
+    });
+}
+
+//will update the page information with the new stats
+var updateInfo = function(index){
+    //get a degree icon
+    // $('span').addClass('degreeSym')
+    //     .text('&#8457');
+
+    // //fist fill in the banner information
+    // $('.city-temp').text = week[i].temp + $('.degreeSym') + " F";
+    // $('.city-wind').text = week[i].wind + " MPH";
+    // $('.city-humidity').text = week[i].humidity + " %";
+    // $('#index-fav').text =week[i].uvindex;
+
+
+}
+
 //event listener to get text inputed in search
 $("#search-btn").on("click", function(event){
     event.preventDefault();
@@ -116,6 +167,8 @@ $("#search-btn").on("click", function(event){
 
     //immedietely add the search as a button to refer to later
     addToHistory(currentCity);
+    getLocationApi(currentCity);
+    updateInfo();
 });
 
 setDates();
